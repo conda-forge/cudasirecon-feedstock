495bb68747e18ab3a429bbd8c81c7c6ca829e3bf
Talley Lambert <talley.lambert@gmail.com>
2022-12-01 11:06:42 -0500

    update buildsh

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
+if [ ! -d "src/IVE" ] 
+then
+    wget src/ https://www.dropbox.com/s/2twvw0go3dr3aim/IVE.zip
+    unzip -o -d src IVE.zip
+    rm IVE.zip
+fi
 mkdir cmake_build
 cd cmake_build
 
-
 CXXFLAGS="$CXXFLAGS -Wno-deprecated-declarations"
-cmake ${CMAKE_ARGS} -DBUILD_MRC=OFF -DCMAKE_BUILD_TYPE=Release ../src
+cmake ${CMAKE_ARGS} \
+    -DBUILD_MRC=ON \
+    -DBUILD_OTF_VIEWER=OFF \
+    -DCMAKE_BUILD_TYPE=Release \
+    ../src
+
 make -j${CPU_COUNT}
 make install
