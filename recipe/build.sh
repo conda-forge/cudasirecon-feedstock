57085c948dc972b2d9746880a534a98d91956423
Talley Lambert <talley.lambert@gmail.com>
2022-12-03 14:46:09 -0500

    add unzip

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,8 +2,11 @@
 if [ ! -d "src/IVE" ] 
 then
     wget src/ https://www.dropbox.com/s/2twvw0go3dr3aim/IVE.zip
+    echo "IVE downloaded"
     unzip -o -d src IVE.zip
+    echo "IVE unzipped"
     rm IVE.zip
+    echo "IVE.zip removed"
 fi
 mkdir cmake_build
 cd cmake_build
